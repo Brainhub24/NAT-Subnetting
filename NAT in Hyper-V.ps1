3ac1a27d3b6741498622d030cdb8e36c5f8f1f1a
Jason Prime <600613.m6@gmail.com>
2023-01-26 14:43:24 +0100

    Translate to English
    Headlines/Comments simply translated into English. Nothing else...

--- NAT in Hyper-V.ps1
+++ NAT in Hyper-V.ps1
@@ -1,51 +1,35 @@
-﻿
-#                                         Drei Schritte sind für ein NAT-Subnetz notwendig:
+
+# Three steps are necessary for a NAT subnet:
 #
-#1. Erzeugen eines internen Switch im Hyper-V.
-#2. Festlegen einer IP-Adresse für den internen Switch. Diese IP-Adresse stellt dann das Standard-Gateway für das NAT-Subnetz dar.
-#3. Erzeugen eines NetNat Objekts, mit dem das NAT-Subnetz definiert wird und das später die Netzwerk-Adressübersetzung vornimmt.
+# 1. Create an internal switch in Hyper-V.
+# 2. Set an IP address for the internal switch. This IP address then represents the default gateway for the NAT subnet.
+# 3. Create a NetNat object that will be used to define the NAT subnet and that will later perform the network address translation.
 
 
 
 
-#Internen Switch erstellen
+# Create an internal switch
 
 New-VMSwitch -SwitchName NATSwitch12 -SwitchType Internal
 
 
-#Name oder Index der Netzwerkschnittstellen herausbekommen.
+# Get the name or index of the network interfaces.
 
 
 Get-NetAdapter
 
-#Und dann Config des neuen Internen Switches
+# And then Config the new Internal Switch
 
 New-NetIPAddress 192.168.12.1 -PrefixLength 24 -InterfaceIndex 12
 
 
 
 
-#Erzeugen eines NetNat Objekts
+# Create a NetNat object
 
-New-NetNat –Name “NatSwitch12” -InternalIPInterfaceAddressPrefix "192.168.12.0/24"
+New-NetNat -Name "NatSwitch12" -InternalIPInterfaceAddressPrefix "192.168.12.0/24"
 
 
-#Kontrolle ob NAT-Switch OK
+# Check if NAT switch is OK
 
 Get-NetNat
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
